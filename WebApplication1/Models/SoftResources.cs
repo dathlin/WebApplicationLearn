6d15a5f925c1a53dc15d80a7834f5f664ebc6f79
dathlin <hsl200909@163.com>
2017-05-21 09:56:35 +0800

    添加项目文件。

---
+++ WebApplication1/Models/SoftResources.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+
+    /****************************************************************************************
+     * 
+     *    该类放置了一些应用程序的相关基础配置
+     * 
+     * 
+     * 
+     * 
+     *****************************************************************************************/
+
+
+
+
+
+
+
+
+    /// <summary>
+    /// 软件共享资源
+    /// </summary>
+    public class SoftResources
+    {
+        public static readonly string ApplicationName = "Richard.Hu Application";
+    }
+}
