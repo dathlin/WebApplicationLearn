cb09ca780cee0e4c25e0f0f8a41c229203b72305
dathlin <hsl200909@163.com>
2017-05-27 23:28:21 +0800

    更新nuget包和添加一个数据库的模型

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace WebApplication1.Controllers
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-
+            
             return View();
         }
 
